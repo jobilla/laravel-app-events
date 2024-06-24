06b51e6124331e75a4835e63d70b9753e3a77a42
Vladimir Andronov <vladimir.andronov@gmail.com>
2024-06-24 15:21:55 +0300

    Add size limit to the app event messages

--- src/AppEvent.php
+++ src/AppEvent.php
@@ -2,16 +2,18 @@
 
 namespace Decahedron\AppEvents;
 
+use Google\Protobuf\Internal\Message;
 use Illuminate\Bus\Queueable;
 use Illuminate\Container\Container;
-use Illuminate\Support\Facades\App;
-use Google\Protobuf\Internal\Message;
-use Illuminate\Support\Facades\Config;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Config;
 
 class AppEvent implements ShouldQueue
 {
     use Queueable;
+
+    const MAX_PAYLOAD_SIZE = 1024 * 1024; // 1 MB
 
     /**
      * @var string
@@ -25,14 +27,21 @@ class AppEvent implements ShouldQueue
 
     /**
      * Event constructor.
+     *
      * @param string $event
      * @param        $payload
      */
-    public function __construct(string $event, Message $payload)
+    public function __construct(string $event, Message $payload, int $sizeLimit = null)
     {
+        $sizeLimit = $sizeLimit ?? self::MAX_PAYLOAD_SIZE;
+
+        if (strlen($payload->serializeToJsonString()) > $sizeLimit) {
+            throw new \Exception('Payload size exceeds the limit of ' . $sizeLimit . ' bytes');
+        }
+
         $this->onConnection('app-events');
         $this->payload = $payload;
-        $this->event = $event;
+        $this->event   = $event;
     }
 
     public function handle()
